31f86cc57e13f8658aeda7ed4802c5c87620bd0c
Higor Tavares <paulohtfreire@gmail.com>
2024-09-22 07:20:55 -0300

    chore: refactor

--- events/first.js
+++ events/first.js
@@ -6,13 +6,14 @@ const pub = new MyPublisher();
 const newEvent = "new";
 const crashEvent = "crash"
 
-pub.on(newEvent, (argument)=> {
-    console.log(`new event has been trigged! ${argument}`)
-})
-pub.on(crashEvent, ()=> {
+const newEventListener = (arg) => { console.log(`new event has been trigged! ${arg}`); }
+const crashEventListener = () => {
     console.log("crash event has been trigged! exit...")
     process.exit(1)
-})
+}
+
+pub.on(newEvent, newEventListener);
+pub.on(crashEvent, crashEventListener)
 
 pub.emit(newEvent, "hello,");
 pub.emit(newEvent, "world!");
